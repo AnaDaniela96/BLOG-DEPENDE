6e82526b2625f4ebfcbae26e9684b8b7c047feb8
Daniela <danielaandrade66@gmail.com>
2025-03-27 11:38:43 -0600

    Add react router dom for create routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies"
+
+import Navbar from "./components/navbar";
+
+
 function App() {
-  return <h1>Hola mundo</h1>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/peliculas" element={<Movies />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'; 
 
 /* main.jsx sólo se encarga de montar App.jsx en el DOM */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
-  </StrictMode>,
+  </StrictMode>
 )
 
 
