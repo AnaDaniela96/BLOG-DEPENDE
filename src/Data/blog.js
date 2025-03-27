299f3599d81841ab761d9701f3cd4aa136f8421f
Daniela <danielaandrade66@gmail.com>
2025-03-27 11:33:33 -0600

    Update data and create new examples.

---
+++ src/Data/blog.js
@@ -0,0 +1,42 @@
+export const posts = [
+  {
+    id: 1,
+    title: "Mi primera entrada",
+    content: "Este es el contenido de la entrada.",
+    tags: ["personal", "inicio"],
+    date: "2025-03-27",
+    assets: ["imagen1.jpg"],
+  },
+  {
+    id: 2,
+    title: "Cómo construí mi blog",
+    content: "Hablemos sobre mi proceso.",
+    tags: ["tecnología", "desarrollo"],
+    date: "2025-03-28",
+    assets: ["imagen2.jpg"],
+  },
+  {
+    id: 3,
+    title: "Te gusta La filosofía",
+    content: "Hablemos sobre mi proceso.",
+    tags: ["tecnología", "desarrollo"],
+    date: "2025-03-28",
+    assets: ["imagen2.jpg"],
+  },
+  {
+    id: 4,
+    title: "Prefiero no decir nada",
+    content: "Hablemos sobre mi proceso.",
+    tags: ["tecnología", "desarrollo"],
+    date: "2025-03-28",
+    assets: ["imagen2.jpg"],
+  },
+  {
+    id: 5,
+    title: "Mi primer texto sobre minimalismo digital",
+    content: "Hablemos sobre mi proceso.",
+    tags: ["tecnología", "desarrollo"],
+    date: "2025-03-28",
+    assets: ["imagen2.jpg"],
+  },
+];
---
+++ src/Data/movies.js
@@ -0,0 +1,268 @@
+export const movies = [
+    {
+        id:0,
+        name: "Nosferatu",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Robert Eggers",
+        },
+            year: 2024,
+        country: [
+            "República Checa",
+            "Estados Unidos"
+        ],
+        genre: [
+            "Terror gótico",
+            "Vampiros"
+        ],
+        calification: {
+            imdb: "7.2/10",
+            filmaffinity: "6.8/10",
+            daniela: "4"
+        },
+        comment: "Una reimaginación oscura y atmosférica del clásico de terror, con actuaciones inquietantes y una estética visual impresionante.",
+        url_imagen: '/img/Nosferatu.jpg',
+        url_trailer: "https://www.ejemplo.com/nosferatu_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 1,
+        name: "Anora",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Sean Baker",
+        },
+            year: 2024,
+        country: ["Estados Unidos"],
+        genre: ["Drama", "Comedia"],
+        calification: {
+            imdb: "7.8/10",
+            filmaffinity: "7.2/10",
+            daniela: "4.5"
+        },
+        comment: "Una historia cruda y emotiva sobre la vida de una joven en Nueva York, con la firma de Sean Baker en su estilo realista y naturalista.",
+        url_imagen: "/img/anora.jpg",
+        url_trailer: "https://www.ejemplo.com/anora_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 2,
+        name: "Confesiones de una típica adolescente",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Sara Sugarman",
+        },
+            year: 2004,
+        country: ["Estados Unidos"],
+        genre: ["Comedia", "Juvenil"],
+        calification: {
+            imdb: "5.2/10",
+            filmaffinity: "5.0/10",
+            daniela: "3"
+        },
+        comment: "Un coming-of-age con Lindsay Lohan que mezcla música, drama adolescente y el clásico enfrentamiento con la popularidad escolar.",
+        url_imagen: "/img/CTA.jpg",
+        url_trailer: "https://www.ejemplo.com/confesiones_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 3,
+        name: "Mientras dormías",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Jon Turteltaub",
+        },
+            year: 1995,
+        country: ["Estados Unidos"],
+        genre: ["Comedia romántica"],
+        calification: {
+            imdb: "6.7/10",
+            filmaffinity: "6.4/10",
+            daniela: "4"
+        },
+        comment: "Un romance entrañable con Sandra Bullock que juega con la idea del amor inesperado y la identidad.",
+        url_imagen: "/img/Mientras_dormias.jpg",
+        url_trailer: "https://www.ejemplo.com/mientras_dormias_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 4,
+        name: "Y tu mamá también",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Alfonso Cuarón",
+        },
+            year: 2001,
+        country: ["México"],
+        genre: ["Drama", "Road movie"],
+        calification: {
+            imdb: "7.6/10",
+            filmaffinity: "7.8/10",
+            daniela: "5"
+        },
+        comment: "Un viaje iniciático cargado de sensualidad y crítica social, con una dirección impecable de Cuarón.",
+        url_imagen: "/img/ytumamatambien.jpg",
+        url_trailer: "https://www.ejemplo.com/y_tu_mama_tambien_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 5,
+        name: "Cónclave",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Edward Berger",
+        },
+            year: 2024,
+        country: ["Reino Unido"],
+        genre: ["Drama", "Thriller político"],
+        calification: {
+            imdb: "7.4/10",
+            filmaffinity: "7.1/10",
+            daniela: "4"
+        },
+        comment: "Intriga y poder se entremezclan en este thriller sobre la elección de un nuevo Papa.",
+        url_imagen: "/img/conclave.jpg",
+        url_trailer: "https://www.ejemplo.com/conclave_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 6,
+        name: "Algo azul",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Mariel García Spooner",
+        },
+            year: 2021,
+        country: ["Panamá"],
+        genre: ["Comedia romántica"],
+        calification: {
+            imdb: "6.0/10",
+            filmaffinity: "5.8/10",
+            daniela: "3.5"
+        },
+        comment: "Una historia divertida sobre bodas y expectativas sociales, con un giro inesperado.",
+        url_imagen: "/img/algoAzul.jpg",
+        url_trailer: "https://www.ejemplo.com/algo_azul_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 7,
+        name: "Mike and Dave: Los busca novias",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Jake Szymanski",
+        },
+            year: 2016,
+        country: ["Estados Unidos"],
+        genre: ["Comedia"],
+        calification: {
+            imdb: "6.0/10",
+            filmaffinity: "5.5/10",
+            daniela: "3"
+        },
+        comment: "Un desmadre total con Zac Efron y Adam Devine, repleto de humor absurdo y situaciones caóticas.",
+        url_imagen: "/img/mikeanddave.jpg",
+        url_trailer: "https://www.ejemplo.com/mike_dave_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 8,
+        name: "Pretty Woman",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Garry Marshall",
+        },
+            year: 1990,
+        country: ["Estados Unidos"],
+        genre: ["Comedia romántica"],
+        calification: {
+            imdb: "7.1/10",
+            filmaffinity: "6.8/10",
+            daniela: "4.5"
+        },
+        comment: "Un clásico romántico con Julia Roberts y Richard Gere, que redefinió las historias de amor en los 90.",
+        url_imagen: "/img/Pretty_Woman.jpg",
+        url_trailer: "https://www.ejemplo.com/pretty_woman_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 9,
+        name: "John Tucker Must Die",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Betty Thomas",
+        },
+            year: 2006,
+        country: ["Estados Unidos"],
+        genre: ["Comedia juvenil"],
+        calification: {
+            imdb: "5.8/10",
+            filmaffinity: "5.5/10",
+            daniela: "3"
+        },
+        comment: "Venganza adolescente con humor y estilo, en un plan para desenmascarar al galán infiel.",
+        url_imagen: "/img/john.jpg",
+        url_trailer: "https://www.ejemplo.com/john_tucker_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 10,
+        name: "All About Steve",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Phil Traill",
+        },
+            year: 2009,
+        country: ["Estados Unidos"],
+        genre: ["Comedia romántica"],
+        calification: {
+            imdb: "4.8/10",
+            filmaffinity: "4.5/10",
+            daniela: "2.5"
+        },
+        comment: "Sandra Bullock en una comedia peculiar sobre el amor no correspondido y la perseverancia.",
+        url_imagen: "/img/steve.jpg",
+        url_trailer: "https://www.ejemplo.com/all_about_steve_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 11,
+        name: "The Heat",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Paul Feig",
+        },
+            year: 2013,
+        country: ["Estados Unidos"],
+        genre: ["Acción", "Comedia"],
+        calification: {
+            imdb: "6.6/10",
+            filmaffinity: "6.2/10",
+            daniela: "4"
+        },
+        comment: "Sandra Bullock y Melissa McCarthy en una buddy-cop comedy llena de risas y acción.",
+        url_imagen: "/img/theheat.jpg",
+        url_trailer: "https://www.ejemplo.com/the_heat_trailer.mp4",
+        IseeIn: 2025,
+    },
+    {
+        id: 12,
+        name: "New York Stories",
+        director: {
+            directorPhoto: "/img/robertEgers.webp",
+            directorName: "Martin Scorsese, Francis Ford Coppola, Woody Allen",
+        },
+            year: 1989,
+        country: ["Estados Unidos"],
+        genre: ["Drama", "Antología"],
+        calification: {
+            imdb: "6.4/10",
+            filmaffinity: "6.2/10",
+            daniela: "4"
+        },
+        comment: "Tres relatos únicos que capturan la esencia de Nueva York a través de distintos estilos cinematográficos.",
+        url_imagen: "/img/newyork.jpg",
+        url_trailer:"https://www.youtube.com/embed/mqNoTUstmDc?si=yVyZ-nx9PmzYWYIl",
+        IseeIn: 2025,
+    }
+]
