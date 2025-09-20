454fab015629a0d664ef700af27511f04527ebb6
androideanadaniela <androideanadaniela>
2025-09-20 17:08:40 -0600

    feat: stylos home and navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import BlogList from "@/features/blog/pages/BlogList";
 import About from "@/pages/About";
 import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
-import Movies from "@/features/play/pages/Movies";
-import MovieDetail from "@/features/play/pages/MovieDetail";
-import PlayIndex from "./features/play/pages/PlayIndex";
+import Movies from "@/features/play/movies/pages/Movies";
+import MovieDetail from "@/features/play/movies/pages/MovieDetail";
+import PlayIndex from "@/features/play/pages/PlayIndex";
 
 import Navbar from "@/components/navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -30,11 +31,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
 
-      <footer className="border-t">
-        <div className="max-w-6xl mx-auto px-4 py-6 text-sm text-gray-600">
-          © {new Date().getFullYear()} Daniela — hecho con React + Tailwind
-        </div>
-      </footer>
+      <Footer />
     </BrowserRouter>
   );
 }
